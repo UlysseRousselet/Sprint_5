60cf555685386eb899649671442871feb9800a36
Ulysse Rousselet <ulysse.rousselet@epitech.eu>
2026-04-02 20:09:12 +0200

    add: multiplayer

---
+++ Source/Sprint_5/Sprint_5.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Sprint_5.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Sprint_5, "Sprint_5" );
